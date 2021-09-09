fa3de305bcd75ccf416789a668a3120527f5561a
Josh <jkstadtm@uark.edu>
2021-09-09 14:48:07 -0500

    some of the basic files

---
+++ hw1.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import nltk
+
+
+# inputFile = input("Enter a .html file to read: ")
+# outputFile = input("Name a file for the parsed txt to live in: ")
+
+
+# #f = open(inputFile, "r")
+# #print(f.read())
+
+# with open(inputFile, mode='r') as in_file, \
+#      open(outputFile, mode='w') as out_file:
+
+#     # A file is iterable
+#     # We can read each line with a simple for loop
+#     for line in in_file:
+#         for word in line.split():
+#             out_file.write(word)
+#             out_file.write("\n")
+# f = open(outputFile, mode="r")
+# print("reading outputFile ",outputFile, "\n", f.read())
+
+
+directory = input("input name of directory: ")
+file = input("input name of file within directory: ")
+print(os.listdir(directory))
+f = open(os.getcwd()+"/"+directory+"/"+file, "r")
+print(f.read())
+print("CWD: ",os.getcwd())
+print('done')
